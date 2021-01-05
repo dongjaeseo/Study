7fe85f80042d57c6c97a423bebf266529cf425f6
DongJae Seo <75518803+dongjaeseo@users.noreply.github.com>
2021-01-05 20:03:36 +0900

    01.05 homework

---
+++ practice/practice5_LSTM_DNN.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+#1. data
+x = np.array([1,2,3,2,3,4,3,4,5,4,5,6,5,6,7,6,7,8,7,8,9,8,9,10,9,10,11,10,11,12,20,30,40,30,40,50,40,50,60])
+y = np.array([4,5,6,7,8,9,10,11,12,13,50,60,70])
+x_pred = np.array([50,60,70])
+
+x = x.reshape(13,3)
+x_pred = x_pred.reshape(1,3)
+
+from sklearn.preprocessing import MinMaxScaler
+scale = MinMaxScaler()
+scale.fit(x)
+x = scale.transform(x)
+x_pred = scale.transform(x_pred)
+
+#2. 모델링
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense, Input
+input = Input(shape = (3,))
+d = Dense(100, activation = 'relu')(input)
+d = Dense(50)(d)
+d = Dense(50)(d)
+d = Dense(50)(d)
+d = Dense(1)(d)
+
+model = Model(inputs = input, outputs = d)
+model.summary()
+
+#3. 컴파일 훈련
+# from tensorflow.keras.callbacks import EarlyStopping
+# early = EarlyStopping(monitor = 'loss', patience = 30)
+
+model.compile(loss = 'mse', optimizer = 'adam')
+model.fit(x,y,batch_size =1, epochs = 600, verbose = 2)
+
+#4. 평가예측
+loss = model.evaluate(x,y,batch_size =1)
+print('loss : ', loss)
+y_pred = model.predict(x_pred)
+print(y_pred)
+
+# [[79.94561]]
