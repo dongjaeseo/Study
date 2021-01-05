49a1c6ad69285cbd020f4c69283a6a07da5ba0fa
DongJae Seo <75518803+dongjaeseo@users.noreply.github.com>
2021-01-05 18:53:09 +0900

    01.05 final

--- keras/keras27_LSTM_DNN.py
+++ keras/keras27_LSTM_DNN.py
@@ -1,3 +1,8 @@
+# keras23_LSTM_scale DNN으로 코딩
+
+# DNN으로 23번파일보다 loss를 좋게 만들것
+
+
 import numpy as np
 
 x = np.array([[1,2,3], [2,3,4], [3,4,5], [4,5,6], [5,6,7],
@@ -10,11 +15,9 @@ from sklearn.preprocessing import MinMaxScaler
 scale = MinMaxScaler()
 scale.fit(x)
 x = scale.transform(x)
+# x_test = scale.transform(x_test)
+# x_val = scale.transform(x_val)
 x_pred = scale.transform(x_pred)
-
-x = x.reshape(13,3,1)
-x_pred = x_pred.reshape(1,3,1)
-
 
 #2. 모델링
 
@@ -22,9 +25,9 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, LSTM
 
 model = Sequential()
-model.add(LSTM(100, activation= 'relu', input_shape = (3,1)))
-model.add(Dense(20))
-model.add(Dense(20))
+model.add(Dense(100, activation= 'relu', input_dim = 3))
+model.add(Dense(30))
+model.add(Dense(30))
 model.add(Dense(30))
 model.add(Dense(1))
 
@@ -35,11 +38,20 @@ model.add(Dense(1))
 # ear = EarlyStopping(monitor = 'loss', patience= 10, mode = 'auto')
 
 model.compile(loss = 'mse', optimizer = 'adam')
-model.fit(x,y,epochs = 200, batch_size = 1)
+model.fit(x,y,epochs = 600, batch_size = 1, verbose =2)
 
 #4. 평가 예측
+loss = model.evaluate(x,y,batch_size = 1)
 y_pred = model.predict(x_pred)
+print('loss : ', loss)
 print(y_pred)
 
+# LSTM
 # [[80.0424]]
 # [[79.88142]]
+
+# DNN
+# loss :  0.007085837423801422
+# [[79.973465]]
+# loss :  0.0006358188111335039
+# [[80.006775]
