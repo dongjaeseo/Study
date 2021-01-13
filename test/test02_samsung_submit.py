15ccfe863a5d8ff0105330600f4d76eb6016e8f0
DongJae Seo <75518803+dongjaeseo@users.noreply.github.com>
2021-01-14 01:41:10 +0900

    01.14 삼성주가

--- test/test02_samsung_submit.py
+++ test/test02_samsung_submit.py
@@ -5,17 +5,15 @@ import pandas as pd
 x_train = np.load('../data/npy/ss_x_train.npy')
 x_test = np.load('../data/npy/ss_x_test.npy')
 x_val = np.load('../data/npy/ss_x_val.npy')
-x_pred = np.load('../data/npy/ss_x_pred.npy') # 01.13 일의 데이터, 01.14 의 종가를 예측하기 위함
-
+x_pred = np.load('../data/npy/ss_x_pred.npy')
 y_train = np.load('../data/npy/ss_y_train.npy')
 y_test = np.load('../data/npy/ss_y_test.npy')
 y_val = np.load('../data/npy/ss_y_val.npy')
 
 #2. 모델
-from tensorflow.keras.models import Sequential, load_model
-from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, Dropout, LSTM
+from tensorflow.keras.models import load_model
 
-model = load_model('../data/modelcheckpoint/samsungjuga_ 47.hdf5')
+model = load_model('../data/modelcheckpoint/samsungjuga_190-3634096.hdf5')
 
 #4. 평가 예측
 result = model.evaluate(x_test,y_test,batch_size = 8)
@@ -24,9 +22,9 @@ y_pred = model.predict(x_test)
 from sklearn.metrics import r2_score
 print('r2 : ', r2_score(y_test,y_pred))
 
-y_pred2 = model.predict(x_pred)
-print(y_pred2)
+y_next = model.predict(x_pred)
+print(y_next)
 
-# loss, mae :  [2562125.5, 1141.66455078125]
-# r2 :  0.9661172643063053
-# [[91059.47]]
+# loss, mae :  [1278777.0, 902.4755859375]
+# r2 :  0.9794304449840759
+# [[96138.62]]
