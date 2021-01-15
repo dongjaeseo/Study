0f09bd021ec0ee79bc32b6ede673e93afd274ee8
DongJae Seo <75518803+dongjaeseo@users.noreply.github.com>
2021-01-15 15:25:14 +0900

    01.15 samsung_merge_data_function

--- samsung/samsung_submit.py
+++ samsung/samsung_submit.py
@@ -1,22 +1,22 @@
 import numpy as np
 import pandas as pd
+from samsung_merge_function import size
 
 #1. 데이터
-x_train = np.load('./samsung/samsung.npz')['x_train']
-x_test = np.load('./samsung/samsung.npz')['x_test']
-x_val = np.load('./samsung/samsung.npz')['x_val']
-x_pred = np.load('./samsung/samsung.npz')['x_pred']
-y_train = np.load('./samsung/samsung.npz')['y_train']
-y_test = np.load('./samsung/samsung.npz')['y_test']
-y_val = np.load('./samsung/samsung.npz')['y_val']
+x_train = np.load('./samsung/samsung_new_%d.npy'%size,allow_pickle=True)[0]
+x_test = np.load('./samsung/samsung_new_%d.npy'%size,allow_pickle=True)[1]
+x_val = np.load('./samsung/samsung_new_%d.npy'%size,allow_pickle=True)[2]
+y_train = np.load('./samsung/samsung_new_%d.npy'%size,allow_pickle=True)[3]
+y_test = np.load('./samsung/samsung_new_%d.npy'%size,allow_pickle=True)[4]
+y_val = np.load('./samsung/samsung_new_%d.npy'%size,allow_pickle=True)[5]
+x_pred = np.load('./samsung/samsung_new_%d.npy'%size,allow_pickle=True)[6]
 
 #2. 모델
 from tensorflow.keras.models import load_model
-
-model = load_model('./samsung/samsungjuga.hdf5')
+model = load_model('./samsung/samsungjuga_0115.hdf5')
 
 #4. 평가 예측
-result = model.evaluate(x_test,y_test,batch_size = 1)
+result = model.evaluate(x_test,y_test,batch_size = 8)
 print('loss, mae : ', result)
 y_pred = model.predict(x_test)
 from sklearn.metrics import r2_score
@@ -25,15 +25,5 @@ print('r2 : ', r2_score(y_test,y_pred))
 y_next = model.predict(x_pred)
 print(y_next)
 
-# 전후 데이터 다 쓴모델
-# r2 :  0.9937017091301253
-# [[88493.24]]
-# loss, mae :  [1166993.375, 766.7100830078125]
-# r2 :  0.9933660531669145
-# [[91902.34]]
-# loss, mae :  [1181757.625, 841.8319091796875]
-# r2 :  0.9936705864431818
-# [[91872.766]]
-# loss, mae :  [2145229.75, 1073.52099609375]
-# r2 :  0.9886229686283687
-# [[91100.86]]
+##############################
+
