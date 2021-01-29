59f15a96aff24f025596682da9fe327788605b34
DongJae Seo <75518803+dongjaeseo@users.noreply.github.com>
2021-01-29 09:33:04 +0900

    0129 수업전

--- ml/m10_kfold_3_hw.py
+++ ml/m10_kfold_3_hw.py
@@ -17,35 +17,22 @@ dataset = load_iris()
 x = dataset.data
 y = dataset.target
 
-x_train, x_test, y_train, y_test = train_test_split(x,y, shuffle = True, train_size = 0.8)
-
 kfold = KFold(n_splits = 5, shuffle = True)
-
-# from sklearn.preprocessing import MinMaxScaler
-# scale = MinMaxScaler()
-# scale.fit(x_train)
-# x_train = scale.transform(x_train)
-# x_test = scale.transform(x_test)
-
-from sklearn.preprocessing import StandardScaler
-scale = StandardScaler()
-scale.fit(x_train)
-x_train = scale.transform(x_train)
-x_test = scale.transform(x_test)
 
 #2. modelling
 model = RandomForestClassifier()
+for train_idx, val_idx in kfold.split(x,y): 
+    # train fold, val fold 분할
+    x_train = x[train_idx]
+    x_test = x[train_idx]
+    y_train = y[train_idx]
+    y_test = y[train_idx]
 
-scores = cross_val_score(model, x_train, y_train, cv = kfold)
-print('scores : ', scores)
-# #3. compile fit
-# model.fit(x_train,y_train)
+    scores = cross_val_score(model, x_train, y_train, cv = kfold)
+    print('score : ', scores)
 
-# #4. evaluation, prediction
-# result = model.score(x_test,y_test)
-# print('model_score : ', result)
-# y_pred = model.predict(x_test)
-# acc = accuracy_score(y_pred,y_test)
-# print('accuracy_score : ', acc)
-
-# scores :  [1.         1.         0.91666667 0.875      0.95833333]
+# score :  [0.91666667 0.91666667 0.91666667 0.95833333 0.95833333]
+# score :  [1.         1.         0.875      0.95833333 0.91666667]
+# score :  [0.91666667 0.95833333 0.95833333 0.95833333 1.        ]
+# score :  [0.95833333 1.         0.91666667 1.         0.875     ]
+# score :  [0.91666667 0.95833333 1.         0.95833333 1.        ]
--- ml/m12_GridSearch2_1_iris.py
+++ ml/m12_GridSearch2_1_iris.py
@@ -5,7 +5,7 @@
 import numpy as np
 from sklearn.datasets import load_iris
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
-from sklearn.model_selection import train_test_split, KFold, cross_val_score, GridSearchCV
+from sklearn.model_selection import train_test_split, KFold, cross_val_score, GridSearchCV, RandomizedSearchCV
 from sklearn.metrics import accuracy_score
 
 from sklearn.ensemble import RandomForestClassifier
@@ -41,14 +41,26 @@ scale.fit(x)
 x = scale.transform(x)
 
 #2. modelling
+import datetime
+t1 = datetime.datetime.now()
 model = GridSearchCV(RandomForestClassifier(), parameters, cv = kfold)
+model.fit(x_train,y_train)
+t2 = datetime.datetime.now()
+model = RandomizedSearchCV(RandomForestClassifier(), parameters, cv = kfold)
+model.fit(x_train,y_train)
+t3 = datetime.datetime.now()
 # scores = cross_val_score(model, x_train, y_train, cv = kfold)
 
 # #3. compile fit
-model.fit(x_train,y_train)
+print('Grid 걸린 시간 : ', t2-t1)
+print('Random 걸린 시간 : ', t3-t2)
 
-# #4. evaluation, prediction
-print('최적의 매개변수 : ', model.best_estimator_)
 
-y_pred = model.predict(x_test)
-print('최종정답률', accuracy_score(y_test,y_pred))
+# # #4. evaluation, prediction
+# print('최적의 매개변수 : ', model.best_estimator_)
+
+# y_pred = model.predict(x_test)
+# print('최종정답률', accuracy_score(y_test,y_pred))
+
+# Grid 걸린 시간 :  0:01:27.178281
+# Random 걸린 시간 :  0:00:06.405835
