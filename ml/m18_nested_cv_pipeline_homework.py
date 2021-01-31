547533003cf623dc11ae00123ceb95956021919c
DongJae Seo <75518803+dongjaeseo@users.noreply.github.com>
2021-01-31 23:41:44 +0900

    0131 2340

--- ml/m18_nested_cv_pipeline_homework.py
+++ ml/m18_nested_cv_pipeline_homework.py
@@ -20,17 +20,23 @@ y = dataset.target
 kfold = KFold(n_splits = 5, shuffle = True)
 
 parameters = [
-    {'n_estimators' : [100,200], 'max_depth' : [6,8,10,12], 'min_samples_leaf' : [3,5,7,10], 'min_samples_split' : [2, 3, 5, 10], 'n_jobs' : [-1]}
+    {'randomforestclassifier__n_estimators' : [100,200], 'randomforestclassifier__max_depth' : [6,8,10,12], 'randomforestclassifier__min_samples_leaf' : [3,5,7,10], 'randomforestclassifier__min_samples_split' : [2, 3, 5, 10]}
 ]
 
-pipe = make_pipeline(MinMaxScaler(),RandomForestClassifier())
 
 #2. modelling
-model = RandomizedSearchCV(RandomForestClassifier(), parameters, cv = kfold)
+pipe = make_pipeline(MinMaxScaler(),RandomForestClassifier())
+model = RandomizedSearchCV(pipe, parameters, cv = kfold)
+model.fit(x,y)
 
+pipe2 = make_pipeline(MinMaxScaler(),model)
 score = cross_val_score(model, x, y, cv = kfold) # 그리드서치에서 최적의 값이 나온걸 또 5번 돌려서
 
 print('교차검증점수 : ', score)
-# scores = cross_val_score(model, x_train, y_train, cv = kfold)
 
-# 교차검증점수 :  [1.         0.95833333 0.875      0.95833333 0.95833333]
+# 교차검증점수 :  [1.         0.96666667 0.93333333 0.93333333 1.        ]
+# 뭔가 했는데 맞는건지 모르겠다!!
+# 파이프라인이 그냥 모델에 적용하기전 스케일을 해준다고 가정하고
+# 먼저 랜덤서치 하기전에 파이프라인을 만들어서 스케일된 x에 최적인 모델을 찾아 model 에 저장했다!
+
+# 두번째 파이프라인은 찾은 최적의 모델+스케일을 해서 교차검증하였다!!
--- ml/m22_FI_RF1_test_iris.py
+++ ml/m22_FI_RF1_test_iris.py
@@ -1,13 +1,22 @@
 # 랜포
 # 피처 임포턴스가25% 미만인 아덜 빼고
 
-from sklearn.tree import RandomForestClassifier
-from sklearn.datasets import load_breast_cancer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+dataset = load_iris()
+x = dataset.data
+y = dataset.target
+
+x_train,x_test,y_train,y_test = train_test_split(x,y,train_size = 0.8)
+
+model1 = RandomForestClassifier(max_depth = 4)
+model1.fit(x_train,y_train)
+print('컬럼 지우기 전 스코어!! : ', model1.score(x_test,y_test))
 
 def plot_feature_importances_dataset(model, dataset, feature):
     n_features = dataset.data.shape[1]
@@ -19,3 +28,25 @@ def plot_feature_importances_dataset(model, dataset, feature):
     plt.xlabel("Feature Importances")
     plt.ylabel("Features")
     plt.ylim(-1, n_features)
+
+# plot_feature_importances_dataset(model1, dataset, 1)
+# plt.show()
+
+#################################################################################################
+columns = model1.feature_importances_
+print('각각의 피쳐 임포턴스!! : ', columns)
+print(dataset.feature_names)
+quantile = pd.DataFrame(columns).quantile(q = 0.25)
+df = pd.DataFrame(x).copy()
+
+j = 0
+numbers = []
+for i in columns:
+    if i < quantile[0]:
+        print(f'{dataset.feature_names[j]} 는 하위 25퍼센트이므로 삭제!')
+        numbers.append(j)
+    j += 1
+    
+df = df.drop(df.columns[numbers], axis =1)
+print(df)
+print(dataset.data)
