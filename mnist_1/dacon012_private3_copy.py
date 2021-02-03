1092fa95853421b182ba55bcce1a5047da1f1322
DongJae Seo <75518803+dongjaeseo@users.noreply.github.com>
2021-02-03 20:02:28 +0900

    0203 2002

--- mnist_1/dacon012_private3_copy.py
+++ mnist_1/dacon012_private3_copy.py
@@ -8,6 +8,7 @@ from keras import Sequential,Model
 from keras.layers import *
 from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 from keras.optimizers import Adam
+from scipy import stats
 
 
 
@@ -94,13 +95,13 @@ sub = pd.read_csv('./mnist_1/submission.csv', header = 0)
 
 # drop columns
 train2 = train.drop(['id','digit','letter'],1)
-train2[train2<40] = 0
+# train2[train2<40] = 0
 
 train2_ens = train2.copy()
 train2_ens[train2_ens<192] /= 2
 
 test2 = test.drop(['id','letter'],1)
-test2[test2<40] = 0
+# test2[test2<40] = 0
 
 test2_ens = test2.copy()
 test2_ens[test2_ens<192] /= 2
@@ -133,7 +134,7 @@ skf = StratifiedKFold(n_splits=40, random_state=42, shuffle=True)
 reLR = ReduceLROnPlateau(patience=100,verbose=1,factor=0.5) #learning rate scheduler
 es = EarlyStopping(patience=160, verbose=1)
 
-result = 0
+result = []
 nth = 0
 
 for train_index, valid_index in skf.split(train2,train['digit']) :
@@ -149,20 +150,99 @@ for train_index, valid_index in skf.split(train2,train['digit']) :
     
     train_generator = idg.flow([x_train,x_train_ens],y_train,batch_size=8)
     valid_generator = idg2.flow([x_valid,x_valid_ens],y_valid)
-    test_generator = idg2.flow([test2,test2_ens],shuffle=False)
     
-    model = model()
+    input1 = Input(shape = (28,28,1))
+    d1 = Conv2D(16,3,activation = 'relu', padding = 'same')(input1)
+    d1 = BatchNormalization()(d1)
+    d1 = Dropout(0.3)(d1)
+
+    d1 = Conv2D(32, 3, activation='relu', padding='same')(d1)
+    d1 = BatchNormalization()(d1) 
+    d1 = Conv2D(32, 5, activation='relu', padding='same')(d1)
+    d1 = BatchNormalization()(d1)
+    d1 = Conv2D(32, 5, activation='relu', padding='same')(d1)
+    d1 = BatchNormalization()(d1)
+    d1 = Conv2D(32, 5, activation='relu', padding='same')(d1)
+    d1 = BatchNormalization()(d1)
+    d1 = MaxPooling2D(3)(d1)
+    d1 = Dropout(0.3)(d1)
+
+    d1 = Conv2D(64, 3, activation='relu', padding='same')(d1)
+    d1 = BatchNormalization()(d1)
+    d1 = Conv2D(64, 5, activation='relu', padding='same')(d1)
+    d1 = BatchNormalization()(d1)
+    d1 = MaxPooling2D(3)(d1)
+    d1 = Dropout(0.3)(d1)
+
+    d1 = Flatten()(d1)
+
+    d1 = Dense(128, activation='relu')(d1)
+    d1 = BatchNormalization()(d1)
+    d1 = Dropout(0.3)(d1)
+    d1 = Dense(32, activation='relu')(d1)
+
+
+    input2 = Input(shape = (28,28,1))
+    d2 = Conv2D(16,3,activation = 'relu', padding = 'same')(input2)
+    d2 = BatchNormalization()(d2)
+    d2 = Dropout(0.3)(d2)
+
+    d2 = Conv2D(32, 3, activation='relu', padding='same')(d2)
+    d2 = BatchNormalization()(d2) 
+    d2 = Conv2D(32, 5, activation='relu', padding='same')(d2)
+    d2 = BatchNormalization()(d2)
+    d2 = Conv2D(32, 5, activation='relu', padding='same')(d2)
+    d2 = BatchNormalization()(d2)
+    d2 = Conv2D(32, 5, activation='relu', padding='same')(d2)
+    d2 = BatchNormalization()(d2)
+    d2 = MaxPooling2D(3)(d2)
+    d2 = Dropout(0.3)(d2)
+
+    d2 = Conv2D(64, 3, activation='relu', padding='same')(d2)
+    d2 = BatchNormalization()(d2)
+    d2 = Conv2D(64, 5, activation='relu', padding='same')(d2)
+    d2 = BatchNormalization()(d2)
+    d2 = MaxPooling2D(3)(d2)
+    d2 = Dropout(0.3)(d2)
+
+    d2 = Flatten()(d2)
+
+    d2 = Dense(128, activation='relu')(d2)
+    d2 = BatchNormalization()(d2)
+    d2 = Dropout(0.3)(d2)
+    d2 = Dense(32, activation='relu')(d2)
+
+    d3 = concatenate([d1,d2])
+    d3 = Dense(32, activation='relu')(d3)
+    d3 = BatchNormalization()(d3)
+    d3 = Dropout(0.3)(d3)
+    d3 = Dense(16, activation='relu')(d3)
+    d3 = BatchNormalization()(d3)
+    d3 = Dropout(0.3)(d3)
+    output = Dense(10, activation='softmax')(d3)
+
+    model = Model(inputs = [input1,input2], outputs = output)
 
     model.compile(loss='sparse_categorical_crossentropy', optimizer=Adam(lr=0.002,epsilon=None),metrics=['acc'])
     
-    learning_history = model.fit_generator(train_generator,epochs=2000, validation_data=valid_generator, callbacks=[es,mc,reLR])
+    learning_history = model.fit_generator(train_generator,epochs=10000, validation_data=valid_generator, callbacks=[es,mc,reLR])
     
     # predict
     model.load_weights('best_cvision.h5')
-    result += model.predict_generator(test_generator,verbose=True)/40
-    
-    nth += 1
-    print(nth, '번째 학습을 완료했습니다.')
+    y_pred = model.predict([test2,test2_ens])
+    y_pred = y_pred.argmax(1)
+    result.append(y_pred)
+result = np.array(result)
+mode = stats.mode(result).mode
+mode = np.transpose(mode)
+sub.loc[:, 'digit'] = mode
 
-sub['digit'] = result.argmax(1)
-sub.to_csv('./mnist_1/submission12.csv',index=False)
+sub.to_csv('./mnist_1/submission12.csv', index = 0)
+
+count = stats.mode(result).count
+for i in count[0]:
+    print(i)
+    
+
+# sub['digit'] = result.argmax(1)
+# sub.to_csv('./mnist_1/submission12.csv',index=False)
