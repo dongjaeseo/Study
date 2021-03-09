47e10f7420da71c7dccc81f2a23d93bd5c9e04c5
DongJae Seo <75518803+dongjaeseo@users.noreply.github.com>
2021-03-09 12:27:08 +0900

    올렸어용~~~

---
+++ ml/m48_wine_quality1.py
@@ -0,0 +1,126 @@
+# 실습
+# 맹그러봐!!!
+
+from sklearn.datasets import load_wine
+import numpy as np
+import pandas as pd
+
+datasets = load_wine()
+print(datasets.feature_names)
+print(datasets.target_names)
+
+x = datasets.data
+y = datasets.target
+print(x.shape, y.shape) # (178, 13) (178,)
+print(np.unique(y)) # [0 1 2]
+
+wine = pd.read_csv('../data/csv/winequality-white.csv', sep = ';', header = 0)
+print(wine.head())
+print(wine.shape)
+print(wine.describe())
+
+wine_npy = wine.values
+
+x = wine_npy[:, :-1]
+y = wine_npy[:, -1]
+
+y[y<6] = 5
+y[y==6] = 6
+y[y>6] = 7
+
+print(x.shape, y.shape) # (4898, 11) (4898,)
+
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler
+from sklearn.model_selection import train_test_split
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size = 0.8, random_state = 42)
+
+scale = StandardScaler()
+scale.fit(x_train)
+x_train = scale.transform(x_train)
+x_test = scale.transform(x_test)
+print(x_train.shape, x_test.shape) # (3918, 11) (980, 11)
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
+
+# model = KNeighborsClassifier()
+model = RandomForestClassifier()
+model.fit(x_train, y_train)
+score = model.score(x_test, y_test)
+print('score : ', score)
+
+
+
+
+
+'''
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import PowerTransformer, RobustScaler
+from sklearn.preprocessing import OneHotEncoder
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
+
+#1. 데이터
+dataset = pd.read_csv('../data/csv/winequality-white.csv', sep = ';').astype('float32')
+
+x_data = dataset.iloc[:,:-1]
+y_data = dataset.iloc[:,-1]
+y_data[y_data<6] = 0
+y_data[y_data==6] = 1
+y_data[y_data>6] = 2
+print(y_data.value_counts())
+
+# print(np.unique(y_data)) # [3. 4. 5. 6. 7. 8. 9.]
+# print(x_data) # [4898 rows x 11 columns]
+# print(np.min(y_data)) # 3~9 사이로 측정
+
+x = np.array(x_data)
+y = np.array(y_data).reshape(-1, 1)
+
+encoder = OneHotEncoder()
+encoder.fit(y)
+y = encoder.transform(y).toarray()
+
+# print(x.shape, y.shape) (4898, 11) (4898, 7)
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size = 0.8, random_state = 42)
+x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, train_size = 0.8, random_state = 42)
+scale = RobustScaler()
+scale.fit(x_train)
+
+x_train = scale.transform(x_train)
+x_test = scale.transform(x_test)
+x_val = scale.transform(x_val)
+
+#2. 모델링
+model = Sequential()
+model.add(Dense(1024, activation= 'relu', input_shape = (11,)))
+model.add(Dense(512, activation= 'relu'))
+model.add(Dense(256, activation= 'relu'))
+model.add(Dense(128, activation= 'relu'))
+model.add(Dense(64, activation= 'relu'))
+model.add(Dense(32, activation= 'relu'))
+model.add(Dense(16, activation= 'relu'))
+model.add(Dense(3, activation= 'softmax'))
+
+#3. 컴파일 훈련
+es = EarlyStopping(patience = 20)
+lr = ReduceLROnPlateau(factor = 0.25, verbose = 1, patience = 10)
+model.compile(loss = 'categorical_crossentropy', optimizer= 'adam', metrics = ['acc'])
+model.fit(x_train, y_train, batch_size = 16, epochs = 1000, validation_data=(x_val, y_val), callbacks = [es, lr])
+
+#4. 평가
+loss = model.evaluate(x_test, y_test, batch_size= 16)
+print('loss : ', loss[0])
+print('acc : ', loss[1])
+
+# Robust
+# loss :  2.4030537605285645
+# acc :  0.6428571343421936
+
+'''
