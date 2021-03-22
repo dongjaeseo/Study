5a379b7acac18d0c678a03001ced0f48bd2e20c0
DongJae Seo <75518803+dongjaeseo@users.noreply.github.com>
2021-03-22 19:37:53 +0900

    1일 3롯데리아

--- lpd/017_model_60.py
+++ lpd/017_model_60.py
@@ -11,11 +11,14 @@ from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCh
 from scipy import stats
 
 #0. 변수
-filenum = 13
-batch = 6
+filenum = 17
+img_size = 192
+batch = 16
 seed = 42
 dropout = 0.4
 epochs = 1000
+train_dir = '../data/lpd/train_new2'
+test_dir = '../data/lpd/test_new'
 model_path = '../data/model/lpd_{0:03}.hdf5'.format(filenum)
 save_folder = '../data/lpd/submit_{0:03}'.format(filenum)
 sub = pd.read_csv('../data/lpd/sample.csv', header = 0)
@@ -42,20 +45,20 @@ test_gen = ImageDataGenerator(
     horizontal_flip= True
 )
 
-# Found 39000 images belonging to 1000 classes.
+# Found 58000 images belonging to 1000 classes.
 train_data = train_gen.flow_from_directory(
-    '../data/lpd/train_new',
-    target_size = (224, 224),
+    train_dir,
+    target_size = (img_size, img_size),
     class_mode = 'sparse',
     batch_size = batch,
     seed = seed,
     subset = 'training'
 )
 
-# Found 9000 images belonging to 1000 classes.
+# Found 14000 images belonging to 1000 classes.
 val_data = train_gen.flow_from_directory(
-    '../data/lpd/train_new',
-    target_size = (224, 224),
+    train_dir,
+    target_size = (img_size, img_size),
     class_mode = 'sparse',
     batch_size = batch,
     seed = seed,
@@ -64,36 +67,35 @@ val_data = train_gen.flow_from_directory(
 
 # Found 72000 images belonging to 1 classes.
 test_data = test_gen.flow_from_directory(
-    '../data/lpd/test_new',
-    target_size = (224, 224),
+    test_dir,
+    target_size = (img_size, img_size),
     class_mode = None,
     batch_size = batch,
     shuffle = False
 )
-'''
+
 #2. 모델
-eff = EfficientNetB4(include_top = False, input_shape=(224, 224, 3))
+eff = EfficientNetB4(include_top = False, input_shape=(img_size, img_size, 3))
 eff.trainable = True
 
 pretrained = eff.output
-layer_1 = Flatten()(pretrained)
-layer_2 = Dense(2000, activation = 'relu')(layer_1)
-layer_2 = Dropout(dropout)(layer_2)
-layer_3 = Dense(1000, activation = 'relu')(layer_2)
-output = Dense(1000, activation = 'softmax')(layer_3)
+globalpooling = GlobalAveragePooling2D()(pretrained)
+layer_2 = Dense(1000)(globalpooling)
+layer_2 = swish(layer_2)
+output = Dense(1000, activation = 'softmax')(layer_2)
 
 model = Model(inputs = eff.input, outputs = output)
 
 #3. 컴파일 훈련
 model.compile(loss = 'sparse_categorical_crossentropy', optimizer = 'adam', metrics = ['sparse_categorical_accuracy'])
-model.fit(train_data, steps_per_epoch = np.ceil(39000/batch), validation_data= val_data, validation_steps= np.ceil(9000/batch),\
+model.fit(train_data, steps_per_epoch = len(train_data), validation_data= val_data, validation_steps= len(val_data),\
     epochs = epochs, callbacks = [es, cp, lr])
-'''
+
 model = load_model(model_path)
 
 #4. 평가 예측
+'''
 result = []
-
 for tta in range(50):
     print(f'{tta+1} 번째 TTA 진행중 - mode')
     pred = model.predict(test_data, steps = len(test_data))
@@ -114,32 +116,26 @@ for tta in range(50):
             print(f'{tta+1} 반복 중 {i} 번째는 횟수가 {count} 로 {(tta+1)/2.} 미만!')
 '''
 cumsum = np.zeros([72000, 1000])
+count_result = []
 for tta in range(50):
     print(f'{tta+1} 번째 TTA 진행중 - TTA')
     pred = model.predict(test_data, steps = len(test_data)) # (72000, 1000)
     pred = np.array(pred)
-    print(np.argmax(pred, 1))
     cumsum = np.add(cumsum, pred)
-    print(np.argmax(cumsum, 1))
     temp = cumsum / (tta+1)
     temp_sub = np.argmax(temp, 1)
-    print(temp_sub[:10])
     temp_percent = np.max(temp, 1)
-    print(temp_percent[:10])
 
-    
     count = 0
-    print(f'TTA {tta} : {count} 개가 불확실!')
     i = 0
     for percent in temp_percent:
-        count = 0
         if percent < 0.3:
             print(f'{i} 번째 테스트 이미지는 {percent}% 의 정확도를 가짐')
             count += 1
         i += 1
-        
-    print(f'{tta+1} ')
-    print(f'{tta+1} 번째 제출 파일 저장하는 중')
+    print(f'TTA {tta} : {count} 개가 불확실!')
+    count_result.append(count)
+    print(f'기록 : {count_result}')
     sub.loc[:, 'prediction'] = temp_sub
     sub.to_csv(save_folder + '/sample_{0:03}_{1:02}.csv'.format(filenum, (tta+1)), index = False)
-    '''
+    
--- lpd/aaa.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-a = (np.zeros([5,3]))
-b = (np.ones([5,3]))
-print(np.add(a, b))
