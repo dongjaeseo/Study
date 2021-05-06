f033fae1c9ea98cfb629dd8d280ecfbdf50f1c55
DongJae Seo <75518803+dongjaeseo@users.noreply.github.com>
2021-05-06 12:31:05 +0900

    금잔디

--- keras3/keras107_ak_boston.py
+++ keras3/keras107_ak_boston.py
@@ -12,3 +12,25 @@ from sklearn.metrics import r2_score
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 scaler = StandardScaler()
 scaler.fit(x_train)
+x_train = scaler.transform(x_train)
+x_test = scaler.transform(x_test)
+
+model = ak.StructuredDataRegressor(
+    overwrite=True,
+    max_trials=3
+)
+
+from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
+es = EarlyStopping(patience = 6)
+lr = ReduceLROnPlateau(patience = 3, factor = 0.5, verbose = 1)
+filepath = 'C:/data/h5/autokeras/'
+mc = ModelCheckpoint(filepath, save_best_only=True, verbose = 1)
+
+model.fit(x_train, y_train, epochs = 10, validation_split=0.2, callbacks = [es, lr, mc])
+
+results = model.evaluate(x_test, y_test)
+
+print(results)
+
+best_model = model.tuner.get_best_model()
+best_model.save('C:/data/h5/autokeras/keras107.h5')
